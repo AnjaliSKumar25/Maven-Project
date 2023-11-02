44003f71b4b2f3620d0b704071eb81aa21149fae
Anjali Robin <Anjali Robin@LAPTOP-87MD33QO>
2023-11-02 19:37:31 +0530

    First Commit in Maven project

---
+++ firstproject/src/main/java/firstproject/HashMapExample.java
@@ -0,0 +1,22 @@
+package firstproject;
+
+import java.util.*;
+
+public class HashMapExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<Integer,String> map=new HashMap<Integer,String>();   
+		   map.put(1,"Mango");  
+		   map.put(2,"Apple");    
+		   map.put(3,"Banana");    
+		       
+		   System.out.println("Iterating Hashmap");  
+		   for(Map.Entry m : map.entrySet())
+		   {    
+		    System.out.println(m.getKey()+" "+m.getValue());    
+		   }  
+
+	}
+
+}
